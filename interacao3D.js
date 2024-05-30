8182d0521f0b7c24f04a1f945c01455c16451203
MatheusAssisGhost <matheus.assis.ph@gmail.com>
2024-05-29 21:19:51 -0300

    Atualização do site 2

--- interacao3D.js
+++ interacao3D.js
@@ -1,6 +1,6 @@
 let saudacao = window.prompt('Olá, tudo bem? Qual seu nome?')
 
-window.alert(`Olá ${saudacao}, bem vindo à Animatec!`)
+window.alert(`Olá ${saudacao}, bem-vindo à Animatec!`)
 
 
 var outroElemento = document.querySelector(".main_title"); 
