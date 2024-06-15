fe3a362318ee1a4039d315d4287e5053ce658dff
MatheusAssis <matheus.assis.ph@gmail.com>
2024-06-15 01:07:51 -0300

    Atualização background title

--- interacao3D.js
+++ interacao3D.js
@@ -1,10 +1,22 @@
-let saudacao = window.prompt('Olá, tudo bem? Qual seu nome?')
+// let saudacao = window.prompt('Olá, tudo bem? Qual seu nome?')
 
-window.alert(`Olá ${saudacao}, bem-vindo à Animatec!`)
+// window.alert(`Olá ${saudacao}, bem-vindo à Animatec!`)
 
 
-var outroElemento = document.querySelector(".main_title"); 
-outroElemento.textContent = (`Olá, ${saudacao}! Bem-vindo a Animatec`);
-Element.appendChild(text)
+// var outroElemento = document.querySelector(".main_title"); 
+// outroElemento.textContent = (`Olá, ${saudacao}! Bem-vindo a Animatec`);
+// Element.appendChild(text)
 
+class MobileNavBar {
+    constructor(MobileMenu) {
+        this.Menu_burguer_div = document.querySelector(MobileMenu);
+        this.menu_burguer = document.querySelector (menu_burguer);
+        this.div_nav = document.querySelectorAll(div_nav);
+        this.activeClass = "active";
+    }
+
+    addClickEvent(){
+        this.MobileMenu.addEventListener("click")
+    }
+}
 
