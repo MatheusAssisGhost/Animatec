2e2089c8f1de7be5a86bd588861385e063c1de13
MatheusAssisGhost <matheus.assis.ph@gmail.com>
2024-06-17 19:31:10 -0300

    Atualização responsividade

--- interacao3D.js
+++ interacao3D.js
@@ -1,22 +1,5 @@
-// let saudacao = window.prompt('Olá, tudo bem? Qual seu nome?')
-
-// window.alert(`Olá ${saudacao}, bem-vindo à Animatec!`)
-
-
-// var outroElemento = document.querySelector(".main_title"); 
-// outroElemento.textContent = (`Olá, ${saudacao}! Bem-vindo a Animatec`);
-// Element.appendChild(text)
-
-class MobileNavBar {
-    constructor(MobileMenu) {
-        this.Menu_burguer_div = document.querySelector(MobileMenu);
-        this.menu_burguer = document.querySelector (menu_burguer);
-        this.div_nav = document.querySelectorAll(div_nav);
-        this.activeClass = "active";
-    }
-
-    addClickEvent(){
-        this.MobileMenu.addEventListener("click")
-    }
-}
-
+let saudacao = window.prompt('Olá, tudo bem? Qual seu nome?')
+window.alert(`Olá ${saudacao}, bem-vindo à Animatec!`)
+var outroElemento = document.querySelector(".main_title"); 
+outroElemento.textContent = (`Olá, ${saudacao}! Bem-vindo a Animatec`);
+Element.appendChild(text)
