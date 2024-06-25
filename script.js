d2ea2ffa5b66559d4885b123a28e6a5d1dd602f3
MatheusAssisGhost <matheus.assis.ph@gmail.com>
2024-06-25 19:40:56 -0300

    Adição javascript button top

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-$(document).ready(function() { 
-    window.location.href='#QuemSomos';
-    });
+window.onscroll = function() {scrollfunction()};
+    
+function scrollFunction() {
+    const buttonTop = document.getElementById('BackTop');
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        buttonTop.style.display = "block";
+    } else {
+        buttonTop.style.display = "none";
+        
+    }
+}
+
+function topFunction() {
+    window.scrollTo({top:0, behavior: 'smooth'});
+}
